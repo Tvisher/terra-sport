bae375f20330598c7dc5e6957aac792747286c44
Tvisher <Tvisher@mail.ru>
2022-02-01 00:00:52 +0600

    structure fixation

--- src/js/app.js
+++ src/js/app.js
@@ -4,29 +4,32 @@ baseFunction.testWebP();
 
 
 import ModalVideo from 'modal-video';
-import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
+import Swiper, { Navigation, Pagination, EffectFade, Autoplay } from 'swiper';
 
 const firstScreenSlider = new Swiper(".main__slider", {
-    modules: [Navigation, Pagination, EffectFade],
-    slidesPerView: 1,
-    speed: 800,
+    modules: [Navigation, Pagination, EffectFade, Autoplay],
+    speed: 900,
+    loop: true,
+    autoplay: {
+        delay: 4100,
+    },
     effect: 'fade',
     fadeEffect: {
         crossFade: true
     },
-    loop: true,
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
         clickable: true,
         renderBullet: function (index) {
-            return `<span class="swiper-pagination-bullet main__slider-bullet">${index + 1}</span>`
+            return `<span class="swiper-pagination-bullet main__slider-bullet">${index + 1}</span>`;
         }
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
 });
 
 
